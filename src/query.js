59a9401de4f2c878d598828eb57fe53edaa24d79
vv <89388361+vRyanv@users.noreply.github.com>
2022-10-13 20:56:42 +0700

    fix ath

--- src/query.js
+++ src/query.js
@@ -2,14 +2,14 @@ const database = require('../src/database/connect')
 //
 
 
-// database.query(`insert into users(username, password) values ('admin2', '123123')`)
-//     .then((result) => {
-//         console.log(result.rows)
-//         database.end()
-//     })
-
-database.query('select * from users')
+database.query(`insert into users(username, password) values ('admin3', '123123')`)
     .then((result) => {
         console.log(result.rows)
         database.end()
     })
+
+// database.query('select * from users')
+//     .then((result) => {
+//         console.log(result.rows)
+//         database.end()
+//     })
